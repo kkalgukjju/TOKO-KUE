39c818ec3d5ebed9848f787b3054ba186e7a10ae
kkalgukjju <amalianayla03@gmail.com>
2025-06-15 03:40:31 +0700

    toko kue enak

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+
+
+// Klil di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click',function(e){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+})
